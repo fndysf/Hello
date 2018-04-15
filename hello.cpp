eb3123b4e430734b61a6d6e7b1054d76ef94eb68
qiran shi <1664426098@qq.com>
2018-04-16 05:05:16 +0800

    hello

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+/*************************************************************************
+    > File Name: hello.cpp
+    > Author: ma6174
+    > Mail: ma6174@163.com 
+    > Created Time: 一  4/16 05:00:53 2018
+ ************************************************************************/
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"hello"<<endl;
+    return 0;
+}
